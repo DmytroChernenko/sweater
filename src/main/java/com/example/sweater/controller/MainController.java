19de31f0f1c0f53c69bcb5c14d122024264bffea
dmytro <mitresko12@gmail.com>
2018-11-17 21:53:46 +0200

    add author to messages

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -43,7 +43,7 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model
+            @RequestParam String tag, Model model
     ) {
         Message message = new Message(text, tag, user);
 
@@ -51,7 +51,7 @@ public class MainController {
 
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
 
         return "main";
     }
